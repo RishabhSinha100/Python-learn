74016df3f2ebe07974863e8d9201ef17ffb4fcfc
rk5756 <rishabhsinh952@gmail.com>
2025-05-30 00:24:06 +0530

    first commit

---
+++ DataType_1.py
@@ -0,0 +1,17 @@
+age = 40
+print(age)
+
+
+
+# ///////////
+height= 8.9
+print(height+" this is float value")
+
+
+# ////////////
+name="risabh sinha"
+print(name+" this is string")
+
+# ///////////
+is_strudent=True
+print(is_strudent+" this is a boolean value")
